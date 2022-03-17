6719ed07a7f8ff7af39c15004a49c40b1a8ed69e
ysygMhdxw <75829561+ysygMhdxw@users.noreply.github.com>
2022-03-17 20:34:41 +0800

    Initial commit

---
+++ src/main/java/com/example/hangoutspringboot/HanGouTSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.hangoutspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanGouTSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanGouTSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hangoutspringboot/HanGouTSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hangoutspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanGouTSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
